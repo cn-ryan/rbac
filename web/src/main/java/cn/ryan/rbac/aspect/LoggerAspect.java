c561924500b4b04918f4c6a8e8d7ca0971f00482
sjw <rbac@qq.com>
2019-05-16 16:11:42 +0800

    test for username

--- web/src/main/java/cn/ryan/rbac/aspect/LoggerAspect.java
+++ web/src/main/java/cn/ryan/rbac/aspect/LoggerAspect.java
@@ -39,8 +39,6 @@ public class LoggerAspect {
     @AfterThrowing(value = "declareJointPointExpression()",throwing = "e")
     public void afterThrowing(JoinPoint joinPoint,Exception e){
         if(e instanceof BusinessException){
-            SysUser sysUser = (SysUser) SecurityUtils.getSubject().getPrincipal();
-//            joinPoint.get
         }
     }
 
